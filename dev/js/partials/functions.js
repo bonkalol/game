f3576dfbaac688b976e3a9fa19ef31717e144e26
Bogdan Blinnikov <bonkalol@list.ru>
2015-06-21 21:58:07 +0300

    working on height ipad bug

--- dev/js/partials/functions.js
+++ dev/js/partials/functions.js
@@ -60,26 +60,28 @@ functions.js - включает в себя различные вспомога�
 
 
 // 4.
-(function () {
+// (function () {
 
-	var iOS = navigator.userAgent.match(/(iPod|iPhone|iPad)/);
+// 	var iOS = navigator.userAgent.match(/(iPod|iPhone|iPad)/);
 
-	if( iOS ) {
+// 	if( iOS ) {
 
-		var body = document.querySelector('body'),
-			html = document.querySelector('html');
-		// WORKAROUND: converting 90vh to px
-		function fixMobileSafariViewport() {
-			body.style.height = window.innerHeight + 'px';
-			html.style.height = window.innerHeight + 'px';
-		}
+// 		var body = document.querySelector('body'),
+// 			html = document.querySelector('html'),
+// 			main = document.querySelector('.main');
+// 		// WORKAROUND: converting 90vh to px
+// 		function fixMobileSafariViewport() {
+// 			body.style.height = window.innerHeight + 'px';
+// 			html.style.height = window.innerHeight + 'px';
+// 			main.style.height = window.innerHeight + 'px';
+// 		}
 
-		// listen to portrait/landscape changes
-		window.addEventListener('orientationchange', fixMobileSafariViewport, true);
-		fixMobileSafariViewport();
+// 		// listen to portrait/landscape changes
+// 		window.addEventListener('orientationchange', fixMobileSafariViewport, true);
+// 		fixMobileSafariViewport();
 
 
-	}
+// 	}
 
 
-})();
+// })();
